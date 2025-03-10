d0be9cee3798a598960053862b3cdcafec9c4b0b
Peter Radko <cmtvpetya@gmail.com>
2025-03-10 09:37:27 +0300

    Pass Erudit language to heading element

--- packages/erudit/server/plugin/bitran/transpiler.ts
+++ packages/erudit/server/plugin/bitran/transpiler.ts
@@ -9,7 +9,7 @@ import { ERUDIT_SERVER } from '@server/global';
 import { aliasesName } from '@erudit-js/bitran-elements/aliases/shared';
 import { aliasesTranspiler } from '@erudit-js/bitran-elements/aliases/transpiler';
 import { headingName } from '@erudit-js/bitran-elements/heading/shared';
-import { headingTranspiler } from '@erudit-js/bitran-elements/heading/transpiler';
+import { defineHeadingTranspiler } from '@erudit-js/bitran-elements/heading/transpiler';
 import { includeName } from '@erudit-js/bitran-elements/include/shared';
 import { includeTranspiler } from '@erudit-js/bitran-elements/include/transpiler';
 import { linkName } from '@erudit/shared/bitran/link/shared';
@@ -21,7 +21,9 @@ export async function createBitranTranspiler() {
     const defaultTranspilers = {
         [aliasesName]: aliasesTranspiler,
         [includeName]: includeTranspiler,
-        [headingName]: headingTranspiler,
+        [headingName]: defineHeadingTranspiler({
+            language: ERUDIT_SERVER.CONFIG?.language,
+        }),
         [linkName]: linkTranspiler,
     };
 
