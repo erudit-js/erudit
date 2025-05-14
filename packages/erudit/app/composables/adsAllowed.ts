63dc03ebd411b8abd4677621d358edb2754949b3
Peter Radko <cmtvpetya@gmail.com>
2025-05-14 08:50:01 +0300

    Show ads in production if not turned off manually (fix #17)

--- packages/erudit/app/composables/adsAllowed.ts
+++ packages/erudit/app/composables/adsAllowed.ts
@@ -2,7 +2,7 @@ import eruditConfig from '#erudit/config';
 
 export function adsAllowed() {
     if (typeof eruditConfig?.debug?.ads === 'undefined') {
-        return false;
+        return import.meta.dev ? false : true;
     } else if (!Boolean(eruditConfig?.debug?.ads)) {
         return false;
     }
