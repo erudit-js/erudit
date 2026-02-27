5593043fc218fee04573e27a29ad851d0e33e569
Peter Radko <cmtvpetya@gmail.com>
2026-02-27 15:25:14 +0300

    Use correct topic part icon for global/book nav

--- packages/erudit/app/components/aside/major/contentNav/items/ContentNavTopic.vue
+++ packages/erudit/app/components/aside/major/contentNav/items/ContentNavTopic.vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import { topicParts } from '@erudit-js/core/content/topic';
+
 import ItemTemplate from './ItemTemplate.vue';
 
 const { navItem } = defineProps<{ navItem: FrontContentNavTopic }>();
@@ -7,11 +9,20 @@ const { shortContentId } = useContentId();
 const active = computed(() => {
   return navItem.shortId === shortContentId.value;
 });
+
+const topicIcon = computed(() => {
+  // Article > Summary > Practice
+  if (navItem.parts?.length) {
+    for (const part of topicParts) {
+      if (navItem.parts.includes(part)) return ICONS[part];
+    }
+  }
+});
 </script>
 
 <template>
   <ItemTemplate
-    icon="array-lines"
+    :icon="topicIcon"
     :navItem
     :state="active ? 'active' : undefined"
   />
