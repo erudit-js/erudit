b7042d1e98077ceceb5f27b9c14c065247c1ba4e
Peter Radko <cmtvpetya@gmail.com>
2026-03-24 13:46:23 +0300

    Updated Yandex metrics analytics script code

--- packages/erudit/app/composables/analytics.ts
+++ packages/erudit/app/composables/analytics.ts
@@ -73,17 +73,24 @@ function yandexAnalytics(analytics: YandexAnalytics) {
 (function(m,e,t,r,i,k,a){
     m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
     m[i].l=1*new Date();
-    k=e.createElement(t),a=e.getElementsByTagName(t)[0],
-    k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-})(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+})(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=${analytics.metricsId}', 'ym');
 
-ym(${analytics.metricsId}, "init", {
+ym(
+  ${analytics.metricsId},
+  'init',
+  {
+    ssr:true,
+    webvisor:true,
     clickmap:true,
-    trackLinks:true,
+    referrer: document.referrer,
+    url: location.href,
     accurateTrackBounce:true,
-    webvisor:true
-});
-                    `.trim(),
+    trackLinks:true
+  }
+);
+`.trim(),
         },
       ],
     });
